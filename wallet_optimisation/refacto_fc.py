7a005ed6e08be32aa90a95d6db497ad500d5ebfc
glen <glen84_dr@hotmail.com>
2025-03-19 19:23:42 +0100

    fonction to parse csv data

---
+++ wallet_optimisation/refacto_fc.py
@@ -0,0 +1,24 @@
+import csv
+
+# parsing csv data into list
+def create_actions_list(path:str, missing_profit: bool=True)-> list:
+    actions = []
+    with open(path, 'r') as file:
+        actions_dt = csv.reader(file)
+        next(actions_dt)
+        for action in actions_dt:
+            # changing action price in integer
+            if missing_profit is True:
+                action[1] = int(action[1])
+                calculated_profit = round(calcul_profit(action), 2)
+                action.append(calculated_profit)
+            else:
+                intermed_price = float(action[1]) * 10
+                action[1] = int(intermed_price)
+                if action[1] <= 0:
+                    continue
+                action[2] = float(action[2])
+
+            actions.append(action)
+
+        return actions
