373e5352d3805706f68a484a3770684519088eb6
glen <glen84_dr@hotmail.com>
2025-03-25 13:31:46 +0100

    fibonacci dynamic called from main

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,5 +1,6 @@
 import click, time
 from brutforce import fib
+from memoization import fib_mem
 
 @click.group()
 def cli():
@@ -14,6 +15,15 @@ def execute_fibonacci(n):
     print(f"The fibonacci sequence of {n} is: {fib_nbr}")
     print(f"The time for this execution is: {time.time() - t} seconds")
 
+
+@cli.command()
+@click.argument('n')
+def call_fib_mem(n: int):
+    t = time.time()
+    mem = {}
+    fib_nb = fib_mem(int(n), mem)
+    print(f"The fibonacci sequence of {n} is: {fib_nb}")
+    print(f"The time for this execution is: {time.time() - t} seconds")
 
 if __name__ == '__main__':
     cli()
